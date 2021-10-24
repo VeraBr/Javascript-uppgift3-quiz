679055542c117365cf9a18544176ddd0e477628c
vera-braathen <vera.braathen@hotmail.com>
2021-10-24 20:53:39 +0200

    First version

---
+++ main.js
@@ -0,0 +1,88 @@
+let resultButton = document.querySelector("#check"); 
+
+let radioButtons = document.querySelectorAll('[type="radio"]'); // Radio buttons.
+let checkboxButtons = document.querySelectorAll('[type="checkbox"]'); //Checkbox button.
+
+let container = document.querySelector("#result"); // Element för att skriva ut resultat.
+
+let changeColorButton = document.querySelector("#changeColor");
+
+// Ändrar bakgrunds-och textfärg.
+changeColorButton.addEventListener("click", () => {
+    let body = document.querySelector("body");
+
+    if (body.style.background === "black") {
+        body.style.background = "white";
+        body.style.color = "black";
+        document.getElementById("changeColor").innerHTML = "Dark mode";
+    } else {
+        body.style.background = "black";
+        body.style.color = "lightgrey";
+        document.getElementById("changeColor").innerHTML = "Light mode";
+    }
+});
+
+let totalCorrectAmount = 10; 
+
+resultButton.addEventListener("click", () => {
+    let pointsRadio = 0;
+    let pointsCheckbox = 0;
+
+    radioButtons.forEach(btn => {
+        if (btn.checked && btn.value === "correct") {
+            pointsRadio++;
+        }
+    });
+
+
+    
+    checkboxButtons.forEach(button => {
+        let correctAnswer1 = document.querySelector("input[name='answer1']:checked");
+        let correctAnswer2 = document.querySelector("input[name='answer2']:checked");
+        let wrongAnswer1 = document.querySelector("input[name='answer3']:checked");
+        let wrongAnswer2 = document.querySelector("input[name='answer4']:checked");
+
+        if((correctAnswer1 && correctAnswer2) && (!wrongAnswer1 && !wrongAnswer2)) {
+            console.log(correctAnswer1);
+            pointsCheckbox = 1;
+        }
+    })
+
+    //Plusar ihop radiobutton och checkbox poäng.
+    let totalPoints = pointsRadio + pointsCheckbox;
+
+
+    // Räknar ut hur många rutor som är ifyllda. 
+    let buttonsId = document.querySelectorAll('[id=answer]');
+    let arr = [];
+    for(let i = 0; i < buttonsId.length; i++){
+        let buttonsChecked = buttonsId[i].checked;
+        let count = arr.push(buttonsChecked);
+        console.log("buttons checked: " + buttonsChecked);
+    }
+    let boxesChecked = arr.filter(Boolean).length;
+    console.log("Array: " + arr);
+    console.log("Boxes checked: " + boxesChecked);
+    
+
+
+
+    // Skriver ut poängen i DOMen och ser till att tillräckligt många rutor är ifyllda.
+    if(boxesChecked < 11){
+        alert("checka i alla rutor");
+    } else if (totalPoints > 7){
+        let result = document.createElement("p");
+        result.innerText = `Du fick ${totalPoints} rätt`;
+        result.style.color = "green";
+        container.appendChild(result);
+    } else if (totalPoints > totalCorrectAmount/2) {
+        let result = document.createElement("p");
+        result.innerText = `Du fick ${totalPoints} rätt`;
+        result.style.color = "orange";
+        container.appendChild(result);
+    } else {
+        let result = document.createElement("p");
+        result.innerText = `Du fick ${totalPoints} rätt`;
+        container.appendChild(result);
+    }
+});
